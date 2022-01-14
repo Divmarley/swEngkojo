428b424e7e7e8390ebcff33dac557d17f7fbc1f1
Divmarley <nexted.marley.com>
2022-01-14 18:54:28 +0000

    todays work

--- operators.js
+++ operators.js
@@ -1,7 +1,119 @@
+
+
 /*
 Operators are symbols pthat are used to perform task on 2 or more data values or variable
 
 types of Operators
+1.Arithmetic
+2.Assignment
+3.Comparison
+4.Logical
+5.Type
+Bitwise
+
+
+          1.Arithmetic
+Arithmetic operators perform math..caloperation on numbers 
+Operator	Description
++	         Addition
+-	          Subtraction
+*	         Multiplication
+**	         Exponentiation (ES2016)
+/	         Division
+%	          Modulus (Remainder)
+++	          Increment
+--	          Decrement
+
+
+// let x = 100 + 50;
+// document.write(x);
+// console.log(x);
+
+
+// let k = 100 -50;
+// document.write(k);
+// console.log(k);
+
+
+// let a = 10*50;
+// document.write(a);
+// console.log(a);
+
+
+// let a = 10**2;
+// // 10*10
+// document.write(a);
+// console.log(a);
+
+
+// let a = 10/2; 
+// document.write(a);
+// console.log(a);
+
+
+// let a = 10%3; 
+// document.write(a);
+// console.log(a);
+
+
+// let a = 1; 
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// 
+console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+
+// let a = 1; 
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+
+
+
+// B O D M A S
+ 
+    2.Assignment
+Assignment operators assign values to JavaScript variables.
+
+Operator	Example	Same As
+=	       x = y	x = y
++=	       x += y	x = x + y
+-=	       x -= y	x = x - y
+*=	       x *= y	x = x * y
+/=	       x /= y	x = x / y
+%=	       x %= y	x = x % y
 */ 
 
- 
+
+// let x =4;
+// // console.log(x+=10);
+
+// d = x + 10
+// console.log(d);
+
+
+// k = 10 +43;
+// console.log(k);
+
+let s =4;
+let x ='4';
+console.log(s===x);
+>=
+<=
+!=
+
+
+
