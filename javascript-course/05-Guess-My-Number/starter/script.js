b1b5d66c1f488503f4c44a1c25607faa64fd365e
Divmrley <nexted.marley@gmail.com>
2022-01-26 18:56:27 +0000

    todays class

--- javascript-course/05-Guess-My-Number/starter/script.js
+++ javascript-course/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
-'use strict';
+'use strict'; 
+let secretNumber= Math.trunc(Math.random()*20)+1;
+const checkBtn=document.querySelector('.check');
+const inputfield=document.querySelector('.guess');
+
+const message = document.querySelector('.message') ;
+const mybody = document.querySelector('body') ;
+const hiddenNumber = document.querySelector('.number') ;
+hiddenNumber.innerHTML =secretNumber;
+// mybody.style.backgroundColor='green';
+// mybody.style.backgroundColor='red';
+
+
+function displayMessage(msg) {
+    return  message.innerHTML=msg
+}
+// displayMessage("correct")
+// displayMessage("nt correct")
+
+
+
+checkBtn.addEventListener('click',function() {
+    const guess = inputfield.value;
+    if (!guess) {
+      console.log(  displayMessage("no number"));
+        
+    }
+})
